d505e13f761466f42a92fc62ac85594d67c6c9e5
zoleitschuk <zachary.oleitschuk@gmail.com>
2017-09-02 15:22:15 -0600

    mvp phase 1 for data model

--- vagrant/catalog/models.py
+++ vagrant/catalog/models.py
@@ -1,60 +1,54 @@
-from sqlalchemy import Column,Integer,String
+"""
+Module Docstring
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
-from passlib.apps import custom_app_context as pwd_context
-import random, string
-from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 Base = declarative_base()
-secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
 
-class User(Base):
-    __tablename__ = 'user'
+
+class Category(Base):
+    """
+    Class Docstring here.
+    """
+    __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
-    username = Column(String(32), index=True)
-    password_hash = Column(String(64))
-
-    def hash_password(self, password):
-        self.password_hash = pwd_context.encrypt(password)
-
-    def verify_password(self, password):
-        return pwd_context.verify(password, self.password_hash)
-
-    def generate_auth_token(self, expiration=600):
-    	s = Serializer(secret_key, expires_in = expiration)
-    	return s.dumps({'id': self.id })
-
-    @staticmethod
-    def verify_auth_token(token):
-    	s = Serializer(secret_key)
-    	try:
-    		data = s.loads(token)
-    	except SignatureExpired:
-    		#Valid Token, but expired
-    		return None
-    	except BadSignature:
-    		#Invalid Token
-    		return None
-    	user_id = data['id']
-    	return user_id
-
-class Product(Base):
-    __tablename__ = 'product'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    category = Column(String)
-    price = Column(String)
+    name = Column(String(250), nullable=False)
     @property
     def serialize(self):
-        """Return object data in easily serializeable format"""
+        """
+        Return object data in easily serializeable format
+        """
         return {
-        'name' : self.name,
-        'category' : self.category,
-        'price' : self.price
-            }
+            'id': self.id,
+            'name': self.name,
+        }
 
-engine = create_engine('sqlite:///regalTree.db')
+class Item(Base):
+    """
+    Class Docstring here.
+    """
+    __tablename__ = 'item'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=True)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    @property
+    def serialize(self):
+        """
+        Return object data in easily serializeable format
+        """
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'category_id': self.category_id,
+        }
+
+engine = create_engine('sqlite:///itemCatalog.db')
  
 
 Base.metadata.create_all(engine)
