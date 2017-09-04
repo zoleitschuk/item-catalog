7f2e00222d41c89238a147f6c6cf27bf424003b1
zoleitschuk <zachary.oleitschuk@gmail.com>
2017-09-04 10:56:21 -0600

    use templates for content and add delete urls

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,7 +1,7 @@
 """
 Module docstring here.
 """
-from flask import Flask
+from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base, Category, Item
@@ -16,34 +16,51 @@ session = DBSession()
 
 @app.route('/')
 @app.route('/catalog/')
-def Catalog():
+def catalog():
     """
     Method docstring here.
     """
     categories = session.query(Category).all()
     items = session.query(Item).all()
-    output = '<div><strong>Categories:</strong>'
-    for category in categories:
-        output += category.name
-    output += '</div><div><strong>Items:</strong>'
-    for item in items:
-        output += item.name
-    output += '</div>'
-    return output
-
-@app.route('/item/<int:item_id>/')
-def catalog_item(item_id):
-    """
-    Method docstring here.
-    """
-    return 'Item_id: {}'.format(item_id)
+    return render_template('public_catalog.html', items=items, categories=categories)
 
 @app.route('/category/<int:category_id>/')
 def catalog_category(category_id):
     """
     Method docstring here.
     """
-    return 'Category_id: {}'.format(category_id)
+    # TODO: get full category object and pass into template.
+    return render_template('category.html', category_id=category_id)
+
+@app.route('/category/<int:category_id>/delete/', methods=['GET', 'POST'])
+def delete_category(category_id):
+    """
+    Method docstring here.
+    """
+    # TODO: get full category object and pass into template.
+    if request.method == 'POST':
+        print('DELETE Category requires helper method to be written.')
+    else:
+        return render_template('delete_category.html', category_id=category_id)
+
+@app.route('/item/<int:item_id>/')
+def catalog_item(item_id):
+    """
+    Method docstring here.
+    """
+    # TODO: get full item object and pass into template.
+    return render_template('item.html', item_id=item_id)
+
+@app.route('/item/<int:item_id>/delete/', methods=['GET', 'POST'])
+def delete_item(item_id):
+    """
+    Method docstring here.
+    """
+    # TODO: get full item object and pass into template.
+    if request.method == 'POST':
+        print('DELETE Item requires helper method to be written.')
+    else:
+        return render_template('delete_item.html', item_id=item_id)
 
 if __name__ == '__main__':
     app.debug = True
