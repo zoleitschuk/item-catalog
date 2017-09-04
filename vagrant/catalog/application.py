3d53010be5505893ded354e38ef3c8ead013d45b
zoleitschuk <zachary.oleitschuk@gmail.com>
2017-09-04 11:30:09 -0600

    pass full object to templates rather than just id

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -16,7 +16,7 @@ session = DBSession()
 
 @app.route('/')
 @app.route('/catalog/')
-def catalog():
+def show_catalog():
     """
     Method docstring here.
     """
@@ -25,12 +25,12 @@ def catalog():
     return render_template('public_catalog.html', items=items, categories=categories)
 
 @app.route('/category/<int:category_id>/')
-def catalog_category(category_id):
+def show_category(category_id):
     """
     Method docstring here.
     """
-    # TODO: get full category object and pass into template.
-    return render_template('category.html', category_id=category_id)
+    category = session.query(Category).filter_by(id=category_id).one()
+    return render_template('category.html', category=category)
 
 @app.route('/category/new/', methods=['GET', 'POST'])
 def new_category():
@@ -47,30 +47,30 @@ def edit_category(category_id):
     """
     Method docstring here.
     """
-    # TODO: get full category object and pass into template.
+    edited_category = session.query(Category).filter_by(id=category_id).one()
     if request.method == 'POST':
         print('EDIT Category requires helper method to be written.')
     else:
-        return render_template('edit_category.html', category_id=category_id)
+        return render_template('edit_category.html', category=edited_category)
 
 @app.route('/category/<int:category_id>/delete/', methods=['GET', 'POST'])
 def delete_category(category_id):
     """
     Method docstring here.
     """
-    # TODO: get full category object and pass into template.
+    deleted_category = session.query(Category).filter_by(id=category_id).one()
     if request.method == 'POST':
         print('DELETE Category requires helper method to be written.')
     else:
-        return render_template('delete_category.html', category_id=category_id)
+        return render_template('delete_category.html', category=deleted_category)
 
 @app.route('/item/<int:item_id>/')
-def catalog_item(item_id):
+def show_item(item_id):
     """
     Method docstring here.
     """
-    # TODO: get full item object and pass into template.
-    return render_template('item.html', item_id=item_id)
+    item = session.query(Item).filter_by(id=item_id).one()
+    return render_template('item.html', item=item)
 
 @app.route('/item/new/', methods=['GET', 'POST'])
 def new_item():
@@ -87,22 +87,22 @@ def edit_item(item_id):
     """
     Method docstring here.
     """
-    # TODO: get full item object and pass into template.
+    edited_item = session.query(Item).filter_by(id=item_id).one()
     if request.method == 'POST':
         print('EDIT Item requires helper method to be written.')
     else:
-        return render_template('edit_item.html', item_id=item_id)
+        return render_template('edit_item.html', item=edited_item)
 
 @app.route('/item/<int:item_id>/delete/', methods=['GET', 'POST'])
 def delete_item(item_id):
     """
     Method docstring here.
     """
-    # TODO: get full item object and pass into template.
+    deleted_item = session.query(Item).filter_by(id=item_id).one()
     if request.method == 'POST':
         print('DELETE Item requires helper method to be written.')
     else:
-        return render_template('delete_item.html', item_id=item_id)
+        return render_template('delete_item.html', item=deleted_item)
 
 if __name__ == '__main__':
     app.debug = True
